164fb23647b6dacbf85c420fbc56101970f74451
Bruno Melo <bsdemelodev@hotmail.com>
2024-10-01 16:33:40 -0300

    create show users and add user

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories;
+
+use App\DTO\Users\CreateUserDTO;
+use App\Models\User;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class UserRepository
+{
+    public function __construct(protected User $user) {}
+
+    public function getPaginate(int $totalPerPage = 15, int $page = 1,string $filter = ''): LengthAwarePaginator
+    {
+        return $this->user->where(function ($query) use ($filter) {
+            if ($filter !== '') {
+                $query->where('name', 'LIKE', '%' . $filter . '%');
+            }
+        })->paginate($totalPerPage, ['*'], 'page', $page);
+    }
+
+    public function createNew(CreateUserDTO $dto): User
+    {
+        return $this->user->create((array) $dto); 
+    }
+}
