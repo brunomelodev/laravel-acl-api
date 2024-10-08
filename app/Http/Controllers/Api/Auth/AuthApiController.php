1efe0157574c0e86e8603c7ddb30dd7da6923db7
Bruno Melo <bsdemelodev@hotmail.com>
2024-10-08 17:28:59 -0300

    create Auth Sanctum

---
+++ app/Http/Controllers/Api/Auth/AuthApiController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\AuthApiRequest;
+use App\Http\Resources\UserResource;
+use App\Repositories\UserRepository;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthApiController extends Controller
+{
+    public function __construct(private UserRepository $userRepository)
+    {
+    }
+
+/*************  ✨ Codeium Command ⭐  *************/
+    /**
+     * Authenticate user and return a token.
+     *
+     * @param  \App\Http\Requests\Api\Auth\AuthApiRequest  $request
+     * @return string
+     */
+    
+/******  7e738939-c665-4ff3-bc9b-f2532a900422  *******/
+    public function auth(AuthApiRequest $request)
+    {
+       
+        $user = $this->userRepository->findByEmail($request->email);
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        //antes de criar o toker eu faço o delete para que não exista acesso simultaneo no sistema
+        $user->tokens()->delete();
+
+        $token = $user->createToken($request->device_name)->plainTextToken;
+
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+
+    public function me(){
+
+        $user = Auth::user();
+        return new UserResource($user);
+
+    }
+
+    public function logout()
+    {
+        //tokens gera erro pq estou usando a facade Auth (não tem login autenticado ainda)
+        Auth::user()->tokens()->delete();
+
+        return response()->json(
+            [],
+            Response::HTTP_NO_CONTENT
+        );
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,23 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthApiController;
 use App\Http\Controllers\Api\PermissionController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/permissions', PermissionController::class);
+Route::post('/auth', [AuthApiController::class, 'auth'])->name('auth.login');
+Route::post('/logout', [AuthApiController::class, 'logout'])->name('auth.logout')->middleware('auth:sanctum');
+Route::post('/me', [AuthApiController::class, 'me'])->name('auth.me')->middleware('auth:sanctum');
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('/permissions', PermissionController::class);
+
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+});
 
 Route::get('/', fn() => response()->json(['message' => 'Welcome to the Laravel API.']));
